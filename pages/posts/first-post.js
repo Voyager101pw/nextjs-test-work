c8a2e7eee87efaf318f1252a04f1e72a49271109
Voyager101pw <voyager101sirius@gmail.com>
2022-12-22 21:40:28 +0500

    Metadata: Added Head, title
    See more:
    https://nextjs.org/learn/basics/assets-metadata-css/metadata

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
-  return <>
-    <h1>First post!</h1>
-    <h2>
-      <Link href='/'>Back to home</Link>      
-    </h2>
-  </>
-
-};
+  return (
+    <>
+      <Head>
+        <title>First post</title>
+      </Head>
+      <h1>First post!</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
