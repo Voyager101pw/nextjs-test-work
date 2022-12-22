c60dd1c0a77bb67b0c775c69c8c8829f3d283e16
Voyager101pw <voyager101sirius@gmail.com>
2022-12-23 00:25:16 +0500

    Thrid party script: Added script
    See more:
    https://nextjs.org/learn/basics/assets-metadata-css/third-party-javascript

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>First post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => {
+            console.log('Cкрипт загружен корректно, window.FB заполнен');
+          }}
+        />
       </Head>
       <h1>First post!</h1>
       <h2>
