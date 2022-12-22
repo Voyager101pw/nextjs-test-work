3a6380038612bd93bec10be9bfbaa64c36c0661a
Voyager101pw <voyager101sirius@gmail.com>
2022-12-23 04:00:44 +0500

    Style: Added css layout
    See more:
    https://nextjs.org/learn/basics/assets-metadata-css/layout-component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,24 +1,18 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First post</title>
-        <Script
-          src="https://connect.facebook.net/en_US/sdk.js"
-          strategy="lazyOnload"
-          onLoad={() => {
-            console.log('Cкрипт загружен корректно, window.FB заполнен');
-          }}
-        />
       </Head>
       <h1>First post!</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
