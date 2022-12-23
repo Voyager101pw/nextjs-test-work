a470a3a20c17a4ee1ce0b1098073b0ff2e7bbfd8
Voyager101pw <voyager101sirius@gmail.com>
2022-12-23 20:49:28 +0500

    Style: Added CSS module
    See more:
    https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,74 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+const name = 'Your Name';
+export const siteTitle = 'Next.js Sample Website';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image" // og:image используется для описания содержимого страницы
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {/* home - Логическая опора, которая будет регулировать размер заголовка и изображения. */}
+        {home ? (
+          <>
+            <Image
+              priority  // === true, - изображение будет иметь высокий приоритет и будет 
+                        // предварительно загружено . Ленивая загрузка автоматически отключается 
+                        // для изображений с расширением priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+
+      {/* Ссылка «Вернуться домой» внизу, если homeестьfalse */}
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
 }
