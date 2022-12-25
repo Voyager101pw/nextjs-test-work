539708dfd47ac5cd4b310d93af8b9bc0f6b44128
Voyager101pw <voyager101sirius@gmail.com>
2022-12-25 22:27:01 +0500

    Routes: Added Markdown parser
    See more:
    https://nextjs.org/learn/basics/dynamic-routes/render-markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -16,7 +16,7 @@ export async function getStaticPaths() {
 
 // Вызывается вторым
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -34,6 +34,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
