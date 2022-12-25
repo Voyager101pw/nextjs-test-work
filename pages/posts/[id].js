6e87a567e9763dcee411c61cd8d701c797bdd3c2
Voyager101pw <voyager101sirius@gmail.com>
2022-12-25 21:10:54 +0500

    Routes: Added dynamic route
    See more:
    Step 1 : https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticpaths
    Step 2 : https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// Вызывается первым
+export async function getStaticPaths() {
+  // Должен возвращать обязательную структуру  
+  // Expected: { paths: [], fallback: boolean }
+  // Где paths список все возможных путей для localhost/posts/*.js
+  const paths = getAllPostIds();
+  return {
+    paths, // [ { params: { id }, fallback }, { params: { id }, fallback }]
+           // В params должен обязательно содержатся ключ id
+    fallback: false,
+  };
+}
+
+// Вызывается вторым
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+// Вызывается в послед. очередь
+// How is work: https://nextjs.org/static/images/learn/dynamic-routes/how-to-dynamic-routes.png
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
